741d0b9d99cf8855354e80b68af9312ef707955f
gmluqa <gmluqa@protonmail.com>
2022-10-08 16:24:23 +0200

    Have made the basis of the form handling, now will configure to take the whole payload

--- scripts/player-select.js
+++ scripts/player-select.js
@@ -1,2 +1,10 @@
 sessionStorage.clear()
 
+let playerSelectForm = document.getElementById("player-select-form")
+
+playerSelectForm.addEventListener('submit', function (event) {
+    event.preventDefault()
+    console.log("works")
+})
+
+console.log("script loads")
