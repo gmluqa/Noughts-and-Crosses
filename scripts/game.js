c3debf9333ec6c928590bd187cec0498d04deacc
gmluqa <gmluqa@protonmail.com>
2022-10-09 14:00:42 +0200

    commit before lunch, need to fix action mapping for once turn gets passed

--- scripts/game.js
+++ scripts/game.js
@@ -12,7 +12,7 @@ class TicTacToeMatch {
         this.player2 = player2
         this.player1Name = player1Name
         this.player2Name = player2Name
-        this.board = [["x", "x", "x"], ["x", "o", "o"], ["o", "o", "o"]]
+        this.board = [["", "", ""], ["", "", ""], ["", "", ""]]
         this.turn = 0
         this.activePlayer = ""
         this.action = ""
@@ -22,11 +22,10 @@ class TicTacToeMatch {
 
     StartGame = () => {
         this.turn++
-        this.Player1Turn
+        this.Player1Turn()
     }
 
     Player1Turn = () => {
-        console.log(this.turn)
         this.CheckIf3(1)
         this.activePlayer = this.player1Name
         console.log(this.activePlayer)
@@ -46,23 +45,29 @@ class TicTacToeMatch {
         }
         if (playerNumber == 1 && this.xCount > 2) {
             console.log('player 1 has > 2 x')
+            this.CheckWinner(playerNumber)
             this.action = "remove"
-            return true;
+        } else if (playerNumber == 1) {
+            return this.action = "place"
         }
         if (playerNumber == 2 && this.oCount > 2) {
             console.log('player 2 has > 2 o')
+            this.CheckWinner(playerNumber)
             this.action = "remove"
-            return true;
         }
-        console.log(xCount)
-        console.log(oCount)
+        console.log(this.xCount)
+        console.log(this.oCount)
+    }
+
+    CheckWinner = (playerNumber) => {
+        console.log(`checking if ${playerNumber} has won`)
+
     }
 
     Player2Turn = () => {
-        CheckIf3(2)
+        this.CheckIf3(2)
         this.activePlayer = this.player2Name
-        PlacePiece()
-        Player1Turn()
+        console.log(this.activePlayer)
 
     }
 
@@ -72,27 +77,6 @@ class TicTacToeMatch {
 
 }
 
-// CELL CONTROLLING BUTTONS
-
-let cell00 = document.getElementById("ttt-cell-00")
-let cell01 = document.getElementById("ttt-cell-01")
-let cell02 = document.getElementById("ttt-cell-02")
-let cell10 = document.getElementById("ttt-cell-10")
-let cell11 = document.getElementById("ttt-cell-11")
-let cell12 = document.getElementById("ttt-cell-12")
-let cell20 = document.getElementById("ttt-cell-20")
-let cell21 = document.getElementById("ttt-cell-21")
-let cell22 = document.getElementById("ttt-cell-22")
-
-cell00.onclick = () => { /* ActivePlayer(), DoAction(action) */ }
-cell01.onclick = () => { /* ActivePlayer(), DoAction(action) */ }
-cell02.onclick = () => { /* ActivePlayer(), DoAction(action) */ }
-cell10.onclick = () => { /* ActivePlayer(), DoAction(action) */ }
-cell11.onclick = () => { /* ActivePlayer(), DoAction(action) */ }
-cell12.onclick = () => { /* ActivePlayer(), DoAction(action) */ }
-cell20.onclick = () => { /* ActivePlayer(), DoAction(action) */ }
-cell21.onclick = () => { /* ActivePlayer(), DoAction(action) */ }
-cell22.onclick = () => { /* ActivePlayer(), DoAction(action) */ }
 
 // ASIGNING PLAYER VARS, INITIATE NEW GAME WITH THOSE VARS
 
@@ -106,4 +90,43 @@ let activeGame = new TicTacToeMatch(player1, player2, player1Name, player2Name)
 activeGame.StartGame()
 
 
+// CELL CONTROLLING BUTTONS
 
+let cell00 = document.getElementById("ttt-cell-00")
+let cell01 = document.getElementById("ttt-cell-01")
+let cell02 = document.getElementById("ttt-cell-02")
+let cell10 = document.getElementById("ttt-cell-10")
+let cell11 = document.getElementById("ttt-cell-11")
+let cell12 = document.getElementById("ttt-cell-12")
+let cell20 = document.getElementById("ttt-cell-20")
+let cell21 = document.getElementById("ttt-cell-21")
+let cell22 = document.getElementById("ttt-cell-22")
+
+console.log(` action is ${activeGame.action}`)
+console.log(activeGame.turn)
+cell00.onclick = () => {
+    switch (activeGame.action) {
+        case "place":
+            if (!isEven(activeGame.turn)) {
+                activeGame.board[0][0] = "x"
+                console.log(activeGame.board[0][0])
+                this.turn++
+                activeGame.Player2Turn()
+            }
+
+            break;
+
+        default:
+            break;
+    }
+}
+cell01.onclick = () => { /* ActivePlayer(), DoAction(action) */ }
+cell02.onclick = () => { /* ActivePlayer(), DoAction(action) */ }
+cell10.onclick = () => { /* ActivePlayer(), DoAction(action) */ }
+cell11.onclick = () => { /* ActivePlayer(), DoAction(action) */ }
+cell12.onclick = () => { /* ActivePlayer(), DoAction(action) */ }
+cell20.onclick = () => { /* ActivePlayer(), DoAction(action) */ }
+cell21.onclick = () => { /* ActivePlayer(), DoAction(action) */ }
+cell22.onclick = () => { /* ActivePlayer(), DoAction(action) */ }
+
+const isEven = num => num % 2 === 0;
